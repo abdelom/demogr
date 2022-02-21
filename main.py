326cbcd539ae0294ae9eb0d0e98b7ff8abcefc4b
abdelmajid omarjee <abdelomarjee@hotmailcom>
2022-02-21 15:11:00 +0100

    from migale

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 import pandas as pd
 import numpy as np
+import time
 
 def get_arguments():
     """Retrieves the arguments of the program.
@@ -33,14 +34,18 @@ def main():
         data = pd.read_csv(args.data_ht)
         plt.plot_heatmap(data=data, title="", cbar="", filout=args.output_ht)
     else:
+        start = time.time()
         params = {"sample_size": 10, "Ne": 1, "ro": 8e-3, "mu": 8e-3,  "Tau": 1.0,
         "Kappa": 1.0 , "length": int(1e5), "type": "ld"}
         data_ld = lk.senario(d_type[args.type], params)
+        print(time.time() - start)
         plt.plot_dist(args.type, data_ld,
         "scenario_{}".format(args.type), True)
+        print(time.time() - start)
         kappa_range = np.exp(np.arange(-3.5, 2.8, 0.1))
         tau_range = np.exp(np.arange(-4, 2.3, 0.1))
         data  = lk.data_heat_map(d_type[args.type], kappa_range, tau_range, params)
+        print(time.time() - start)
         data.to_csv(args.output_file, index=False)
     # pkl.dump(generat_senar(params), "out")
      # params = {"sample_size":10, "Ne": 1, "ro": 8e-2, "mu": 8e-3,  "Tau": 1.0, "length": int(1e5)}
