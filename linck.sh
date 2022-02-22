4799da81675ec6bc947d6dc3f2c1439d2a829878
abdelomarjee <abdelomarjee2@gmail.com>
2022-02-22 13:48:29 +0100

    from  abdel

--- linck.sh
+++ linck.sh
@@ -14,8 +14,6 @@
 #$ -e $HOME/work/Err2
 #$ -pe thread 10
 conda activate sei-3.8.5
-python /home/aomarjee/work/demogr/oriented_forest.py
-python /home/aomarjee/work/demogr/oriented_forest_2.py
-#python /home/aomarjee/work/demogr/main.py -t sfs -o /home/aomarjee/work/sfs
-#python /home/aomarjee/work/demogr/main.py -t ld -o /home/aomarjee/work/ld
+python /home/aomarjee/work/demogr/main.py -t sfs -o /home/aomarjee/work/sfs
+python /home/aomarjee/work/demogr/main.py -t ld -o /home/aomarjee/work/ld
 conda deactivate
