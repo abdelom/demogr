1a954d233a5a4b92f38dc016f2b8c75bd8dccd21
abdelmajid omarjee <abdelomarjee@hotmailcom>
2022-03-21 11:07:37 +0100

    migale

--- linck.sh
+++ linck.sh
@@ -1,18 +1,16 @@
-
 #!/bin/bash
 # Nom du job
 # -N msprime
 # Short pour un job < 12h
-#$ -q short.q
+#$ -q long.q
 # Adresse à envoyer
 # -M abdelmajid.omarjee@college-de-france.fr
 # Envoie mail - (b)egin, (e)nd, (a)bort & (s)uspend
 # -m as
 # Sortie standard
-#$ -o $HOME/work/Out2
+#$ -o $HOME/work/linck.out
 # Sortie d'erreur
-#$ -e $HOME/work/Err2
-#$ -pe thread 10
+#$ -e $HOME/work/linck.err
 conda activate sei-3.8.5
 python /home/aomarjee/work/demogr/main.py -t sfs -o /home/aomarjee/work/sfs
 python /home/aomarjee/work/demogr/main.py -t ld -o /home/aomarjee/work/ld
